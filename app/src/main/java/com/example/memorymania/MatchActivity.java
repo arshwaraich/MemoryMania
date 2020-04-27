d20e030f3c3ce550841ad73f260dccd4e4757772
Arshdeep Singh <arshwaraich12@gmail.com>
2020-04-27 03:32:15 -0400

    Double side flip done

--- app/src/main/java/com/example/memorymania/MatchActivity.java
+++ app/src/main/java/com/example/memorymania/MatchActivity.java
@@ -2,10 +2,12 @@ package com.example.memorymania;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
 public class MatchActivity extends AppCompatActivity {
@@ -22,8 +24,15 @@ public class MatchActivity extends AppCompatActivity {
     }
 
     public void flipAnimation(View view) {
-        ObjectAnimator flip = ObjectAnimator.ofFloat(view, "rotationY", 0f, 180f);
-        flip.setDuration(500);
-        flip.start();
+        ImageView imageBack = findViewById(R.id.image_back);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator flip = ObjectAnimator.ofFloat(view, "rotationY", 0f, 90f);
+        flip.setDuration(250);
+        ObjectAnimator flip3 = ObjectAnimator.ofFloat(imageBack, "rotationY", 90f, 180f);
+        flip3.setDuration(250);
+
+        animatorSet.playSequentially(flip,flip3);
+        animatorSet.start();
     }
 }
