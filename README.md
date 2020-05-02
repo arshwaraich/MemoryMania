bbe28b8f54d0e6275e6b7f77a3087feba274e9b8
Arshdeep Singh <arshwaraich12@gmail.com>
2020-05-02 04:53:46 -0400

    Tested sample REST API image loading

---
+++ README.md
@@ -0,0 +1,6 @@
+# Memory Mania
+
+## Third Party Libraries
+1. ![Retrofit](https://square.github.io/retrofit/) - A type-safe HTTP client for Android and Java
+2. ![Converter GSON](https://github.com/square/retrofit/blob/master/retrofit-converters/gson/README.md) - A Converter which uses Gson for serialization to and from JSON.
+3. ![Picasso](https://github.com/square/picasso) - A powerful image downloading and caching library for Android
--- app/src/main/java/com/example/memorymania/MatchActivity.java
+++ app/src/main/java/com/example/memorymania/MatchActivity.java
@@ -4,18 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.memorymania.model.RetroPhoto;
+import com.example.memorymania.network.GetDataService;
+import com.example.memorymania.network.RetrofitClientInstance;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MatchActivity extends AppCompatActivity {
+
+    ProgressDialog progressDoalog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        progressDoalog = new ProgressDialog(MatchActivity.this);
+        progressDoalog.setMessage("Loading....");
+        progressDoalog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<RetroPhoto>> call = service.getAllPhotos();
+        call.enqueue(new Callback<List<RetroPhoto>>() {
+            @Override
+            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
+                progressDoalog.dismiss();
+                String imgURL = response.body().get(0).getThumbnailUrl();
+                TextView textView = findViewById(R.id.api_res_JSON);
+                ImageView imageView = findViewById(R.id.image_back);
+
+                textView.setText(imgURL);
+                Picasso.get().load(imgURL).into(imageView);
+            }
+
+            @Override
+            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
+                progressDoalog.dismiss();
+                Toast.makeText(MatchActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void showResult(View view) {
@@ -29,7 +71,7 @@ public class MatchActivity extends AppCompatActivity {
         AnimatorSet animatorSet = new AnimatorSet();
         ObjectAnimator flip = ObjectAnimator.ofFloat(view, "rotationY", 0f, 90f);
         flip.setDuration(250);
-        ObjectAnimator flip3 = ObjectAnimator.ofFloat(imageBack, "rotationY", 90f, 180f);
+        ObjectAnimator flip3 = ObjectAnimator.ofFloat(imageBack, "rotationY", -90f, 0f);
         flip3.setDuration(250);
 
         animatorSet.playSequentially(flip,flip3);
